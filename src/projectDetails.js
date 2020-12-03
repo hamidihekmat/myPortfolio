b14dc2c646b49737e42d7f02ccb84e22aae8e453
Hekmat Hamidi <theonehamidi@gmail.com>
2020-12-02 22:31:04 -0500

    fixed typos

--- src/projectDetails.js
+++ src/projectDetails.js
@@ -43,7 +43,7 @@ export const projectDetails = [
     name: 'React Player',
     image: 'https://i.imgur.com/FVcd53k.png',
     description:
-      'React Player is a beautiful music player with uses data from Chillhop.com to deliver calm lofi music. In this project I learned to combine Sass to dynamically update css using react state. This project is also mobile friendly.',
+      'React Player is a beautiful music player which uses data from Chillhop.com to deliver calm lofi music. In this project I learned to combine Sass to dynamically update css using react state. This project is also mobile friendly.',
     tools: [tools.react, tools.recoil, tools.css],
     github: 'https://github.com/hamidihekmat/React-Player',
     live: 'https://react-player-tau.vercel.app/',
@@ -53,7 +53,7 @@ export const projectDetails = [
     name: 'Portfolio',
     image: 'https://i.imgur.com/HBCXLSj.png',
     description:
-      'My Portfolio! This was built with ReactJS. It was built with responsiveness in mind. It also has a dark mode option! An easter egg awaits you. ',
+      'My Portfolio! This was built with React. It was built with responsiveness in mind. It also has a dark mode option! An easter egg awaits you. ',
     tools: [tools.react, tools.css, tools.framer],
     github: 'https://github.com/hamidihekmat/myPortfolio',
     live: '',
@@ -74,7 +74,7 @@ export const projectDetails = [
     name: 'Elegant Shop',
     image: 'https://i.imgur.com/mv7CW3U.png',
     description:
-      'Elegant Shop is your number one source for all things and products. Dedicated to providing you the very best of products, with an emphasis on elegant design, cheap and beautiful. In this project I learned to manipulate the DOM with JS.',
+      'Elegant Shop is your number one source for all things and products. Dedicated to providing you the very best of products, with an emphasis on elegant design, affortable and beautiful. In this project I learned to manipulate the DOM with JS.',
     tools: [tools.html, tools.css, tools.js],
     github: 'https://github.com/hamidihekmat/elegantshop',
     live: 'https://elegantshop.netlify.app/',
@@ -84,7 +84,7 @@ export const projectDetails = [
     name: 'Simple Cart',
     image: 'https://i.imgur.com/S075tfL.png',
     description:
-      'Simple Cart is a simple shopping cart app. The purpose for the project was to try and create a shopping cart component and fetch data async within the Cart component. In this project I learned to move data dynamically between components.',
+      'Simple Cart is a shopping cart app. The purpose for the project was to try and create a shopping cart component and fetch data async within the cart component itself. In this project I learned to move data dynamically between components.',
     tools: [tools.react, tools.recoil, tools.css],
     github: 'https://github.com/hamidihekmat/simpleshoppingcart',
     live: 'https://simpleshoppingcart.vercel.app/',
